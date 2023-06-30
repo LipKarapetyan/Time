816ad2fe17b97a8fd5a82483e71b1303e10e224e
Lip  Karapetyan <karapetyanlip.ait@gmail.com>
2023-06-30 19:04:43 +0400

    uodste Clock

--- Clock.jsx
+++ Clock.jsx
@@ -1,28 +1,33 @@
 import { useState,useEffect } from "react";
 
 function Clock() {
-    let time = new Date().toLocaleTimeString()
-    const [timer,setTimer]=useState(time)
-
+    const [time, setTime] = useState(new Date());
+  
     function updateTimer() {
-        
-        time = new Date().toLocaleTimeString()
-        setTimer(time)
+      setTime(new Date());
     }
-
-    useEffect(()=>{
-     let interval = setInterval(()=> updateTimer(),1000)
-     return ()=>{
-        clearInterval(interval)
-     }
-    },[])
-
+  
+    useEffect(() => {
+      let interval = setInterval(() => updateTimer(), 1000);
+      return () => {
+        clearInterval(interval);
+      };
+    }, []);
+  
+    const hours = time.getHours();
+    const minutes = time.getMinutes();
+    const seconds = time.getSeconds();
+  
+    const hoursString = hours < 10 ? "0" + hours : hours.toString();
+    const minutesString = minutes < 10 ? "0" + minutes : minutes.toString();
+    const secondsString = seconds < 10 ? "0" + seconds : seconds.toString();
+  
     return (
-        <div>
-            <h1>Time</h1>
-            <h3>{timer}</h3>
-        </div>
-    )
-}
+      <div>
+        <h1>Time</h1>
+        <h3>{`${hoursString}:${minutesString}:${secondsString}`}</h3>
+      </div>
+    );
+  }
 
-export default Clock
+  export default Clock
